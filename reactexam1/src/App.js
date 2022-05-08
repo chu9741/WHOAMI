455b90d0424941719c39d18902879ecdf3ec36fa
chu9741 <chu9741@gmail.com>
2022-05-08 19:35:38 +0900

    1. import API Test 2. delete lifecycle

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -1,12 +1,35 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
 
 function App() {
   const [data, setData] = useState([]); // data는 빈배열로 시작
+
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime() + 1,
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   const onCreate = (author, content) => {
     const created_date = new Date().getTime();
 
@@ -40,7 +63,6 @@ function App() {
   return (
     <div className="App">
       <h2>일기장</h2>
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} /> {/* DiaryEditor.js import */}
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
