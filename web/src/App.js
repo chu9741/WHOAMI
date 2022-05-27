35a5c3496df03c431851f64b480c81eb955f1d57
chu9741 <chu9741@gmail.com>
2022-05-28 00:24:47 +0900

    init

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import "locomotive-scroll/dist/locomotive-scroll.css";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import GlobalStyles from "./styles/GlobalStyles";
+import { dark } from "./styles/Theme";
+import { useRef } from "react";
+import { AnimatePresence } from "framer-motion";
+
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Insta from "./pages/Insta";
+import ScrollTriggerProxy from "./components/ScrollTriggerProxy";
 
 function App() {
+  const containerRef = useRef(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={dark}>
+        <LocomotiveScrollProvider
+          options={{ smooth: true }}
+          watch={[]}
+          containerRef={containerRef}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <ScrollTriggerProxy />
+          <AnimatePresence>
+            <main className="App" data-scroll-container ref={containerRef}>
+              <Home />
+              <About />
+              <Insta />
+            </main>
+          </AnimatePresence>
+        </LocomotiveScrollProvider>
+      </ThemeProvider>
+    </>
   );
 }
-
 export default App;
---
+++ web/src/styles/Theme.js
@@ -0,0 +1,21 @@
+//Add Themes as per requirements
+
+export const dark = {
+  body: "#202020", //black shade
+  text: "#fff",
+  bodyRgba: "32,32,32",
+  textRgba: "255,255,255",
+
+  grey: "#bebebe",
+
+  fontxs: "0.75em", // 1em = 16px
+  fontsm: "0.875em",
+  fontmd: "1em",
+  fontlg: "1.25em",
+  fontxl: "2em",
+  fontxxl: "3em",
+  fontxxxl: "5em",
+  fontbig: "10em",
+
+  navHeight: "5rem",
+};
