94de7d259ffcc71aacd71b9d92c8340a0573d040
chu9741 <chu9741@gmail.com>
2022-06-01 01:29:50 +0900

    Delete comment

--- web/web/src/index.js
+++ web/web/src/index.js
@@ -1,18 +1,8 @@
 import React from "react";
-// import ReactDOM from "react-dom/client";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-// import reportWebVitals from "./reportWebVitals";
 
-//const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
